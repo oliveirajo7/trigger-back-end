9df7f741e4e57fb3e919b88e7b58f480e2a554a5
Augusto-Filipiak <augustofilipiak@gmail.com>
2025-10-13 19:15:36 -0300

    Testando rotas

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
-
+import roteador from "./Router/Routers.js";
 
 const app = express();
 app.use(express.json());
 
+app.use("/teste", roteador)
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
