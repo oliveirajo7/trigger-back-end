1e7baaccc0710642c1a07440933b2a8c9578b6f3
eoklein <otimosprimevascodagama@gmail.com>
2025-10-20 21:16:53 -0300

    adicionando middleware verificar se é adm

---
+++ src/middlewares/admin.js
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function verifyAdmin(req, res, next) {
+  try {
+    const user = req.user;
+        
+    if (!user) {
+      return res.status(401).json({ mensagem: "Usuário não autenticado" });
+    }
+
+    const usuarioBanco = await prisma.users.findUnique({
+      where: { id: user.id }
+    });
+
+    if (!usuarioBanco) {
+      return res.status(401).json({ mensagem: "Usuário não encontrado" });
+    }
+
+    if (!usuarioBanco.isAdmin) {
+      return res.status(403).json({ mensagem: "Acesso negado: Apenas administradores" });
+    }
+
+    next();
+
+  } catch (error) {
+    console.error("Erro na autorização:", error.message);
+    return res.status(500).json({ mensagem: "Erro na autorização" });
+  }
+}
