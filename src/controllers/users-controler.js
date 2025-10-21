a44b8533f13ed1bd7bbfe70869f3a35157a18356
Joaquim <joaquimoliart123@gmail.com>
2025-10-20 21:00:18 -0300

    Refactor: rota para criar usuario funcionando

--- src/controllers/users-controler.js
+++ src/controllers/users-controler.js
@@ -3,21 +3,25 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 async function createUser(req, res) {
+    const { name, email, age, password, CPF, CEP } = req.body;
 
-    const {name, email, age, password, CPF, CEP} = req.body
+    try {
+        const user = await prisma.users.create({
+            data: {
+                name: name,
+                email: email,
+                age: age,
+                password: password,
+                CPF: CPF,
+                CEP: CEP
+            }
+        });
+        res.status(201).json({ message: "Usuário criado com sucesso", id: user.id });
 
-    const newUser = {
-        name: name,
-        email: email,
-        age: age,
-        password: password,
-        CPF: CPF,
-        CEP: CEP
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Erro ao criar usuário" });
     }
-
-    const createNewUser = prisma.users.create({
-        data: createUser
-    })
-}
+};
 
 export {createUser}
